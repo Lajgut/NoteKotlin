2fa5bb903b19058e9e6e2595ffa6dc377f82d909
kirbit <k.bitkov@mobsolutions.ru>
2017-06-20 19:37:27 +0300

    realm recycler adapter

--- app/src/main/java/com/bravekitty/kir/notekotlin/adapters/NoteAdapter.kt
+++ app/src/main/java/com/bravekitty/kir/notekotlin/adapters/NoteAdapter.kt
@@ -1,5 +1,75 @@
 package com.bravekitty.kir.notekotlin.adapters
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bravekitty.kir.notekotlin.R
+import com.bravekitty.kir.notekotlin.models.NoteModel
+import io.realm.OrderedRealmCollection
+import io.realm.RealmRecyclerViewAdapter
+import kotlinx.android.synthetic.main.item_note.view.*
 
-class NoteAdapter {
+
+class NoteAdapter(
+        private val list: MutableList<NoteModel>,
+        data: OrderedRealmCollection<NoteModel>?,
+        autoUpdate: Boolean
+                            )
+    : RealmRecyclerViewAdapter<NoteModel, NoteAdapter.MyViewHolder>(data, autoUpdate) {
+
+    private var mContext: Context? = null
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        internal fun bind(listIndex: Int) {
+            /**
+             * initialize containers and listeners
+             */
+            //itemView.test.text = list[listIndex].text
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        mContext = parent.context
+        val shouldAttachToParentImmediately = false
+        val adapterView = LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, shouldAttachToParentImmediately)
+        return MyViewHolder(adapterView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        Log.d("onBindViewHolder ", "#" + position)
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    /**
+     * remove 1 element by position
+     */
+    fun removeItem(position: Int) {
+        list.removeAt(position)
+        notifyDataSetChanged()
+    }
+
+    /**
+     * add only 1 element
+     */
+    fun addItem(item: NoteModel) {
+        list.add(item)
+        notifyDataSetChanged()
+    }
+
+    /**
+     * add all elements
+     */
+    fun addAllItem(itemList: List<NoteModel>) {
+        list.addAll(itemList)
+        notifyDataSetChanged()
+    }
 }
